c834c29c30c7a1ff77d2d67285303bac80176496
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-01 01:17:01 +0700

    menambahkan main column

--- app/src/main/java/com/example/tugasprak4/TeksEdit.kt
+++ app/src/main/java/com/example/tugasprak4/TeksEdit.kt
@@ -1,11 +1,17 @@
 package com.example.tugasprak4
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun FormulirPendaftaran(modifier: Modifier = Modifier){
@@ -18,5 +24,14 @@ fun FormulirPendaftaran(modifier: Modifier = Modifier){
     val genderList = listOf("Laki-laki", "Perempuan")
     val statusList = listOf("Janda", "Lajang", "Duda")
 
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF3E5F5)),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
 
 }
