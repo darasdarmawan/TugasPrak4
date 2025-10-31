7a8847b142ad73c903737fbb77f53b39627c586f
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-01 01:03:59 +0700

    menambahkan mutable state untuk input fields

--- app/src/main/java/com/example/tugasprak4/TeksEdit.kt
+++ app/src/main/java/com/example/tugasprak4/TeksEdit.kt
@@ -1,9 +1,19 @@
 package com.example.tugasprak4
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormulirPendaftaran(modifier: Modifier = Modifier){
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+    var isSubmitted by remember { mutableStateOf("") }
+
 
 }
